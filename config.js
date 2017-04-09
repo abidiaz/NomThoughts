c5e4003b4cc07342e5e3d7dc77d05fd69cccd312
Abigail Diaz <github email address>
2017-04-09 02:34:03 -0400

    Created MVP, deleted hello.rb

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'vtWL0aBRazNHepc8sj12K6suL',
+  consumer_secret: 'kmnYznQ91Ijw5d5xpGvYX4BSBKB2THzwuBQBWqpsuNcoutucTF',
+  access_token: '850917081625264128-G7CcKkIBmpI1Abkc6VaDlicEoLwJwkP',
+  access_token_secret: 'edbbgPJXI3eQw4EUeVy6bXhr5gjHjZ3jpgfDNTLw4BD5t'
+}
